48dacd7e78e3526c19e50e6f77fd13af51cedc3a
liucanrui8018 <1163236013liu@gmail.com>
2018-06-01 22:38:17 +0800

    ShoppingApp more

--- usercenter/src/main/java/com/tianyae/usercenter/presenter/RegisterPresenter.kt
+++ usercenter/src/main/java/com/tianyae/usercenter/presenter/RegisterPresenter.kt
@@ -3,29 +3,41 @@ package com.tianyae.usercenter.presenter
 import com.tianyae.baselibrary.ext.execute
 import com.tianyae.baselibrary.presenter.BasePresenter
 import com.tianyae.baselibrary.rx.BaseObserver
-import com.tianyae.usercenter.presenter.service.impl.UserServiceImpl
+import com.tianyae.usercenter.presenter.service.UserService
 import com.tianyae.usercenter.presenter.view.RegisterView
-import io.reactivex.Observer
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
+import javax.inject.Named
 
-class RegisterPresenter : BasePresenter<RegisterView>() {
+class RegisterPresenter @Inject constructor() : BasePresenter<RegisterView>() {
+
+    @Inject
+    @field:[Named("service")]
+    lateinit var userService: UserService
+
 
     fun register(mobile: String, verifyCode: String, pwd: String) {
         /*
         业务逻辑
          */
-        val userService = UserServiceImpl()
+        if(!checkNetWork()) {
 
+            println("网络不可用")
+            return
+        }
 
+        mView.showLoading()
         userService.register(mobile, verifyCode, pwd)
-                .execute(object :BaseObserver<Boolean>() {
-                    override fun onNext(t: Boolean) {
+                .execute(object : BaseObserver<String>(mView) {
+                    override fun onNext(t: String) {
                         mView.onRegisterResult(t)
                     }
-                })
 
+                    override fun onComplete() {
+                        mView.hideLoading()
+
+                    }
+                }, lifecycleProvider)
     }
+
 
 }
