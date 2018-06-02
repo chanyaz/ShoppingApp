9b78f9e13bc118ce9a8d720a449cb08eb717295c
Liucanrui8018 <1163236013@qq.com>
2018-06-02 09:47:24 +0800

    more

--- usercenter/src/main/java/com/tianyae/usercenter/presenter/RegisterPresenter.kt
+++ usercenter/src/main/java/com/tianyae/usercenter/presenter/RegisterPresenter.kt
@@ -5,10 +5,6 @@ import com.tianyae.baselibrary.presenter.BasePresenter
 import com.tianyae.baselibrary.rx.BaseObserver
 import com.tianyae.usercenter.presenter.service.impl.UserServiceImpl
 import com.tianyae.usercenter.presenter.view.RegisterView
-import io.reactivex.Observer
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
 
 class RegisterPresenter : BasePresenter<RegisterView>() {
 
